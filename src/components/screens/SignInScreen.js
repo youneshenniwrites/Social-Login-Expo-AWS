a5c6a34bd2426baa4a15c558a86c90d1b8d466a4
Younes Henni <y.henni@fairfx.com>
2019-07-06 17:49:38 +0100

    feat: replace abc token with facebook token sign in

--- src/components/screens/SignInScreen.js
+++ src/components/screens/SignInScreen.js
@@ -4,19 +4,41 @@ import {
   Text,
   SafeAreaView,
   TouchableOpacity,
+  Alert,
   AsyncStorage
 } from "react-native";
-// import logIn from "../logIn";
+import * as Facebook from "expo-facebook";
+import APP_ID from "../../APP_ID";
+
+const logIn = async props => {
+  try {
+    const { type, token } = await Facebook.logInWithReadPermissionsAsync(
+      APP_ID,
+      {
+        permissions: ["public_profile"]
+      }
+    );
+    if (type === "success") {
+      // Get the user's name using Facebook's Graph API
+      const response = await fetch(
+        `https://graph.facebook.com/me?access_token=${token}`
+      );
+      await AsyncStorage.setItem("userToken", token);
+      props.navigation.navigate("App");
+      Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
+    } else {
+      // type === 'cancel'
+    }
+  } catch ({ message }) {
+    Alert.alert(`Facebook Login Error: ${message}`);
+  }
+};
 
 const SignInScreen = props => {
-  const _signInAsync = async () => {
-    await AsyncStorage.setItem("userToken", "abc");
-    props.navigation.navigate("App");
-  };
   return (
     <SafeAreaView style={styles.container}>
       <Text>Sign In</Text>
-      <TouchableOpacity onPress={_signInAsync}>
+      <TouchableOpacity onPress={() => logIn(props)}>
         <Text>Log in with facebook</Text>
       </TouchableOpacity>
     </SafeAreaView>
